ed3877b6823c4a323a9b4c46c57d465c8bf8c25f
Adrienne Galloway <aasgalloway@gmail.com>
2023-08-25 23:08:46 +0100

    adding empjione convert function to js code

--- interface.js
+++ interface.js
@@ -9,6 +9,12 @@ document.addEventListener("DOMContentLoaded", () => {
     myFunction();
   };
 
+  function convert() {
+    var input = document.getElementById('inputText').value;
+    var output = emojione.shortnameToImage(input);
+    document.getElementById('outputText').innerHTML = output;
+  }
+
   function myFunction() {
     notesApp.addNotes(textBoxElement.value);
     document.getElementById("all-notes").innerHTML +=
@@ -16,9 +22,10 @@ document.addEventListener("DOMContentLoaded", () => {
       document.getElementById("textbox").value=""
   }
 
-  document.getElementById("all-notes").addEventListener("click", showFullNotes);
+  document.getElementById("all-notes").addEventListener("click", showFullNote);
 
-  function showFullNotes() {
+  // showing the full note 
+  function showFullNote() {
     document.getElementById("all-notes").innerHTML = "<ul>" + notesApp.allNotes().join("<br>") + "</ul>";
     // window.location.href = "./full-note-display.html";  
     // document.getElementById("all-notes").innerHTML = notesApp.allNotes();
