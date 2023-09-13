deee391b3e9b1ceeb5da08a141e78fb36466b9e9
Adrienne Galloway <aasgalloway@gmail.com>
2023-09-13 16:30:24 +0100

    emojis showing in notes - import code in index.html

--- interface.js
+++ interface.js
@@ -6,38 +6,22 @@ document.addEventListener("DOMContentLoaded", () => {
   var textBoxElement = document.getElementById("textbox");
 
   document.getElementById("add-note-button").addEventListener("click", newNote);
-  
+
   // being able to type text in the text area, click add and have it show up
   function newNote() {
     notesApp.addNotes(textBoxElement.value);
     document.getElementById("all-notes").innerHTML +=
       "<ul>" + notesApp.abbreviateNote(textBoxElement.value) + "</ul>";
-    document.getElementById("textbox").value = ""
+    document.getElementById("textbox").value = "";
   }
 
   document.getElementById("all-notes").addEventListener("click", showFullNote);
 
   // showing the full note in a new window
   function showFullNote() {
-    document.getElementById("all-notes").innerHTML = "<ul>" + notesApp.allNotes().join("<br>") + "</ul>";
-    // window.location.href = "./full-note-display.html";  
+    document.getElementById("all-notes").innerHTML =
+      "<ul>" + notesApp.allNotes().join("<br>") + "</ul>";
+    // window.location.href = "./full-note-display.html";
     // document.getElementById("all-notes").innerHTML = notesApp.allNotes();
   }
-
-  // Be able to select emojis using the emoji-button
-  const picker = new EmojiButton();
-
-  const button = document.getElementById('emoji-button');
-
-  const textbox = document.getElementById('textbox');
-
-  picker.on('emoji', selection => {
-    // `selection` object has an `emoji` property containing the selected emoji
-    textbox.value += selection.emoji
-    console.log(selection.emoji)
-  });
-  
-  button.addEventListener('click', () => {
-    picker.togglePicker(button);
-  });
 });
