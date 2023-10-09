d6fc81e364616a6c49d70f7fcef15ff99e4c69e9
Adrienne Galloway <aasgalloway@gmail.com>
2023-10-09 15:17:44 +0100

    completing user-story-3

--- interface.js
+++ interface.js
@@ -4,23 +4,23 @@ document.addEventListener("DOMContentLoaded", () => {
   var entryBox = document.getElementById("textbox");
 
   // generate random number for id of note
-  const idGenerator = () =>{
-    return Math.floor(Math.random() * 1000000)
-}
+  const idGenerator = () => {
+    return Math.floor(Math.random() * 1000000);
+  };
 
   // if there are no notes, notesData will be an empty array
   var notesData = JSON.parse(localStorage.getItem("data")) || [];
 
   document.getElementById("add-note-button").addEventListener("click", newNote);
 
-  createNoteNotice()
+  createNoteNotice();
 
   function createNoteNotice() {
     if (notesData.length == 0) {
       let containerDiv = document.querySelector("ul");
       let notesContainer = document.createElement("li");
       containerDiv.appendChild(notesContainer);
-    
+
       let createNoteNotice = `
     <div class="notes">
     <li>
@@ -52,36 +52,40 @@ document.addEventListener("DOMContentLoaded", () => {
     // Save the new note in localStorage
     localStorage.setItem("data", JSON.stringify(notesData));
 
-    // After a new note has been added to localStorage, show all notes 
+    // After a new note has been added to localStorage, show all notes
     showAllNotes();
   }
 
-  function removeElementsByClass(){
+  function removeElementsByClass() {
     const elements = document.getElementsByClassName("notes");
-    while(elements.length > 0){
-        elements[0].parentNode.removeChild(elements[0]);
+    while (elements.length > 0) {
+      elements[0].parentNode.removeChild(elements[0]);
     }
-}
+  }
 
   // Display all the notes from localStorage
   function showAllNotes() {
-    // remove already displayed notes from page 
+    // remove already displayed notes from page
     removeElementsByClass();
-    
+
     let notes = JSON.parse(localStorage.getItem("data"));
 
     notes.forEach((element) => {
-
       let containerDiv = document.querySelector("ul");
-      // let notesContainer = document.createElement("li");
-      // containerDiv.appendChild(notesContainer);
+
+      // cut down note note if it has more than 15 characters
+      if (element.value.length > 15) {
+        var notecontents = element.value.slice(0, 15).concat("...");
+      } else {
+        notecontents = element.value;
+      }
 
       let entryDetails = `
       <div class="notes">
       <li>
         <a class="single-sticky-note" id=${element.timestamp}>
           <p>
-          ${element.value.slice(0, 16)}...
+        ${notecontents}
           </p>
           <div>
           <i class="fa fa-trash" id=${element.id}></i>
@@ -94,54 +98,54 @@ document.addEventListener("DOMContentLoaded", () => {
       document.getElementById("textbox").value = "";
     });
 
-    dynamicDeleteIcon()
-    dynamicSelectIcon()
+    dynamicDeleteIcon();
+    dynamicSelectIcon();
   }
 
   var noteId = [];
   // getElementsByClassName returns an array of elements
   function dynamicDeleteIcon() {
-    var deleteIcons = document.getElementsByClassName("fa fa-trash")
-  // iterate over the array of elements to add an event to each one
+    var deleteIcons = document.getElementsByClassName("fa fa-trash");
+    // iterate over the array of elements to add an event to each one
     for (var i = 0; i < deleteIcons.length; i++) {
       deleteIcons[i].addEventListener("click", (event) => {
-        noteId.push(event.target.id)
-        noteId = Number(noteId.join(''));
-        deleteNote()
+        noteId.push(event.target.id);
+        noteId = Number(noteId.join(""));
+        deleteNote();
       });
     }
-  };
+  }
 
   function deleteNote() {
     var notes = JSON.parse(localStorage.getItem("data"));
-    var noteToDelete = notes.find(item => item.id === noteId);
+    var noteToDelete = notes.find((item) => item.id === noteId);
     var index = notes.indexOf(noteToDelete);
     notes.splice(index, 1);
-    localStorage.setItem('data', JSON.stringify(notes));
+    localStorage.setItem("data", JSON.stringify(notes));
     noteId = [];
 
     if (notes.length == 0) {
       location.reload();
     } else {
-      showAllNotes()
-    };
+      showAllNotes();
+    }
   }
 
-  var noteTimestamp = []
+  var noteTimestamp = [];
 
   function dynamicSelectIcon() {
-    var selectIcons = document.getElementsByClassName("single-sticky-note")
-  // iterate over the array of elements to add an event to each one
+    var selectIcons = document.getElementsByClassName("single-sticky-note");
+    // iterate over the array of elements to add an event to each one
     for (var i = 0; i < selectIcons.length; i++) {
       selectIcons[i].addEventListener("click", (event) => {
-        noteTimestamp.push(event.target.id)
-        noteTimestamp = Number(noteTimestamp.join(''));
-  // Save timestamp of note to localStorage
+        noteTimestamp.push(event.target.id);
+        noteTimestamp = Number(noteTimestamp.join(""));
+        // save timestamp of note to localStorage
         localStorage.setItem("notetocollect", JSON.stringify(noteTimestamp));
         loadNewPage();
       });
     }
-  };
+  }
 
   function loadNewPage() {
     window.location.href = "full-note-display.html";
